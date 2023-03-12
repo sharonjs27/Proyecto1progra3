b95b1deebbec334628d6d6137810d4806162bcd2
sharonjs27 <jimenezsegurasharonstephany@gmail.com>
2023-03-12 16:33:32 -0600

    firts time

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print_hi('PyCharm')
+    print_hi('Perro')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
