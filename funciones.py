95299d2e5905a67222fc78f92efb4f46b29f1504
sharonjs27 <jimenezsegurasharonstephany@gmail.com>
2023-03-13 09:35:36 -0600

    funciones

---
+++ funciones.py
@@ -0,0 +1,10 @@
+def S(N1,N2):
+    return N1 + N2
+def R(a,b):
+    return a - b
+def M(a,b):
+    return a * b
+def D(a,b):
+    return a / b
+def P(a,b):
+    return a % b
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import funciones
+
 
 print(' *** BIENVENIDOS A BASE DE DATOS ***')
 print('1: sumas')
@@ -9,3 +11,18 @@ print('0: Salir')
 
 opcion = input('opcion')
 print(opcion + 'hla')
+N1 = float (input("numero 1"))
+N2 = float (input("numero 2"))
+
+while opcion == 1:
+    print("la suma es" + funciones.S(N1+N2))
+while opcion == 2:
+    print("la resta es",+ funciones.R(N1,N2))
+while  opcion == 3:
+    print("la multiplicacion es", + funciones.M(N1,N2))
+while opcion == 4:
+    print("la divicion es", + funciones.D(N1,N2))
+
+
+
+
