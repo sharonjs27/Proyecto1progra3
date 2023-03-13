ceabd5e01c2d85c813a65e47aac3be9c320b2fd0
sharonjs27 <jimenezsegurasharonstephany@gmail.com>
2023-03-12 22:41:02 -0600

    menu

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-# This is a sample Python script.
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print(' *** BIENVENIDOS A BASE DE DATOS ***')
+print('1: sumas')
+print('2: resta')
+print('3: divicion')
+print('6: multiplicación')
+print('5: tablas de multiplicar')
+print('0: Salir')
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    print_hi('Perro')
-    print_hi('gato')
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+opcion = input('opcion')
+print(opcion + 'hla')
