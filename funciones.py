bf830af4e9d1c3a91732224333abf57c0983eebc
sharonjs27 <jimenezsegurasharonstephany@gmail.com>
2023-03-22 21:54:14 -0600

    funciones horas de trabajo

--- funciones.py
+++ funciones.py
@@ -1,5 +1,19 @@
 import os
 
+# patrón de diseño
+def singleton(cls):
+    instances =dict()
+    def wrap(*args, **kwargs):
+        if cls not in instances:
+            print("llamado instancia  ")
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+class User(object):
+    def __init__(self, username):
+        print("llamado instancia nueva")
+        self.username = username
+
+
 
 class Empleado:
     def __init__(self, nombre, apellido, ocupacion):
@@ -47,85 +61,91 @@ def registrarEmpleado(datos):
         ocupacion = input("Ingrese la ocupación: ")
         nuevoEmpleado = Empleado(nombre, apellido, ocupacion)
 
-        datos.append(nuevoEmpleado)  # agregar los datos ingresados a la lista
-
-
+        datos.append(nuevoEmpleado)  # se guardan los empleados ingresados a la lista
 def listaEmpleados(datos):
     print("Lista de datos ingresados:")
     for nuevoEmpleado in datos:
-        print(f"Empleado {nuevoEmpleado.nombre} {nuevoEmpleado.apellido} ({nuevoEmpleado.ocupacion})")
+        print(f"Empleado {nuevoEmpleado.nombre} Apellido: {nuevoEmpleado.apellido} Ocupación: ({nuevoEmpleado.ocupacion})")
     os.system('pause')
 
 
 def registrarMedicamento(inventario):
     print(" Registro de medicamentos ")
-
-    # el usuario registra los datos
+    # se registra los datos de los medicamentos
     while True:
         nombre = input("Ingrese el nombre (o '0' para terminar): ")
         if nombre == '0':
             break  # si el usuario ingresa '0', se sale del registrar
-
         compActivo = input("Ingrese el compuesto activo: ")
         unidades = input("Ingrese la unidades: ")
         nuevoMedicamento = Medicamentos(nombre, compActivo, unidades)
 
         inventario.append(nuevoMedicamento)  # agregar los datos ingresados a la lista
 
+
 def listaMedicamentos(inventario):
     print("lista de medicamento")
 
     for nuevoMedicamento in inventario:
-        print(f" Medicamento {nuevoMedicamento.nombre} {nuevoMedicamento.compActivo} {nuevoMedicamento.unidades}")
+        print(f" Medicamento {nuevoMedicamento.nombre}Compuesto: {nuevoMedicamento.compActivo} Unidades: {nuevoMedicamento.unidades}")
     os.system('pause')
+
 
 def horasTrabajo():
     print("horas de trabajo")
-    filas = int(input("introduce numero de empleados"))
-    columnas = int(input("introduce numero de columnas"))
+    filas = int(input("Digite número de empleados: "))
+    columnas = int(input("Digite número de días de la semana trabajadas: "))
     horas_trabajadas = [[0, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0]]
-    for i in range(filas):  # Recorre las filas
-        for j in range(columnas):  # Recorre las columnas
+    for i in range(filas):  # Recorre la fila de cada empleado
+        for j in range(columnas):  # Recorre la columna de cada día
             horas_trabajadas[i][j] = int(
                 input(f"Ingrese las horas trabajadas por el empleado {i + 1} el día {j + 1}: "))
 
+    # se suma las horas trabajadas de cada empleado
     for i in range(filas):
         total_horas = sum(horas_trabajadas[i])
         print(f"El empleado {i + 1} trabajó {total_horas} horas esta semana.")
 
+ # se calcula el salario semanal de cada Empleado
     for i in range(filas):
-      salario = (total_horas) * 2000
-      print(f"El empleado {i + 1} tiene un salario de: {salario} esta semana.")
-    # Imprimir la matriz original
+        salario = sum(horas_trabajadas[i]) * 2000
+        print(f"El empleado {i + 1} tiene un salario de: {salario} esta semana.")
 
-    print("Matriz original:")
+        # se calcula el rebajo de seguro
+    for i in range(filas):
+        salario = sum(horas_trabajadas[i]) * 2000
+        seguro = salario - 5000
+        print(f"El empleado {i + 1} tiene un salario de: {seguro} con el seguro rebajado ")
+
+    # muestra los las horas ingresadas
+    print("Horas trabajas: ")
     for i in range(filas):
         for j in range(columnas):
             print(horas_trabajadas[i][j], end=" ")
         print()
 
-    # Pedir al usuario que elija si desea modificar o eliminar un valor de la matriz
-    accion = input("¿Desea modificar o eliminar un valor? (m/e): ")
+    # Solicita al usuario que elija si desea modificar, eliminar o salir
+    accion = input("¿Desea modificar, eliminar un valor o salir? (m/e/s): ")
 
-    # Si elige modificar un valor, pedir al usuario que indique la posición y el nuevo valor
+    # Para modificar un valor, solicitar fila, columna y nuevo valor que desea otorgar
     if accion == "m":
         filas = int(input("Ingrese la fila donde se encuentra el valor que desea modificar: "))
         columnas = int(input("Ingrese la columna donde se encuentra el valor que desea modificar: "))
-        nuevo_valor = int(input("Ingrese el nuevo valor para la posición [{},{}]: ".format(i + 1, j + 1)))
-        horas_trabajadas[filas][columnas] = nuevo_valor
+        nuevo_valor = int(input(f"Ingrese el nuevo valor para la posición [{filas},{columnas}]: "))
+        horas_trabajadas[filas - 1][columnas - 1] = nuevo_valor
 
-    # Si elige eliminar un valor, pedir al usuario que indique la posición
+    # Para eliminar un valor, solicita en que posición el valor que desea eliminar
     elif accion == "e":
         filas = int(input("Ingrese la fila donde se encuentra el valor que desea eliminar: "))
         columnas = int(input("Ingrese la columna donde se encuentra el valor que desea eliminar: "))
-        del horas_trabajadas[i][j]
+        horas_trabajadas[filas-1][columnas-1] = 0
 
-    # Imprimir la matriz modificada
-    print("Matriz modificada:")
+    # Muestra matriz editada con los valores nuevos
+    print("Horas actualizadas:")
     for i in range(len(horas_trabajadas)):
         for j in range(len(horas_trabajadas[0])):
             print(horas_trabajadas[i][j], end=" ")
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import funciones
 
+
+
 datos = []
 inventario = []
+
 menu = ""
 
 while menu != "6":
 
     print('\n*** BIENVENIDO A BASE DE DATO ***')
-    menu = input('MENU \n1: registrar empleado \n2: mostrar lista de empleados \n3: registrar medicamentos \n4: lista de medicamentos \n5: horas trabajadas \n6: Salir \n\n Escriba la opción que desea: ')
+    menu = input('MENU \n1: registrar empleado \n2: mostrar lista de empleados \n3: registrar medicamentos '
+                 '\n4: lista de medicamentos \n5: horas trabajadas \n6: Salir \n\n Escriba la opción que desea: ')
 
     if menu == "1":
+
         funciones.registrarEmpleado(datos)
     elif menu == "2":
         funciones.listaEmpleados(datos)
@@ -19,4 +24,3 @@ while menu != "6":
         funciones.listaMedicamentos(inventario)
     elif menu == "5":
         funciones.horasTrabajo()
-
